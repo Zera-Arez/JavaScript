73ea4eb60323b8b0259e352fb14d95912068aec8
Zera <zeratox7235@gmail.com>
2025-10-03 10:46:27 +0200

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+<script>
+function toggleCompetences() {
+
+const compSection = document.getElementById('competences');
+compSection.style.display =
+compSection.style.display === 'none' ? 'block' : 'none';
+
+}
+</script>
